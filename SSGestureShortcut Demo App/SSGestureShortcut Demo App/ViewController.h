39e9f7d4b081c297c8493b8b85cbd421b632b4f3
Eric Nelson <eric@sandalsoft.com>
2013-05-27 14:10:47 -0500

    Initial commit

---
+++ SSGestureShortcut Demo App/SSGestureShortcut Demo App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SSGestureShortcut Demo App
+//
+//  Created by Eric Nelson on 5/27/13.
+//  Copyright (c) 2013 Sandalsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
